8b76244328bf38ac138f6075e573297a7a104a73
Steven Tegnelia <stegnelia@gmail.com>
2019-06-25 17:35:12 +0900

    yay

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,8 @@
 Rails.application.routes.draw do
+  # get "/new",
+  # get 'articles/articles'
+  # get 'articles/show'
+  # get 'articles/edit'
+  resources :articles
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
